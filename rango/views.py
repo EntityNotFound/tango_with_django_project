c3e07a16f1fda8bf3537bb51e4e4a7ebb5e88c02
Alex Kilkenny <2464370k@student.gla.ac.uk>
2021-02-10 18:34:45 +0000

    Up to task 5

---
+++ rango/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+    
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by <your-name>'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
